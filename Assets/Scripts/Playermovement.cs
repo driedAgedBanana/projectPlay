910924a3df7d27bf53a7dfb278a50325efb17ea5
dryAgedBanana <b.cao@sintlucasedu.nl>
2023-11-29 11:50:40 +0100

    Adding running mechanic

--- Assets/Scripts/Playermovement.cs
+++ Assets/Scripts/Playermovement.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class Playermovement : MonoBehaviour
 {
     public float speed;
-
+    public float runSpeedMultiplier = 2f; // Adjust this value to set the running speed multiplier
+    private bool isRunning = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -19,7 +20,21 @@ public class Playermovement : MonoBehaviour
         float xInput = Input.GetAxis("Horizontal");
         float yInput = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector2.right * xInput * speed * Time.deltaTime);
-        transform.Translate(Vector2.up * yInput * speed * Time.deltaTime);
+        // Check if the shift key is held down
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            isRunning = true;
+        }
+        else
+        {
+            isRunning = false;
+        }
+
+        // Calculate the speed based on whether the player is running or not
+        float currentSpeed = isRunning ? speed * runSpeedMultiplier : speed;
+
+        // Move the player
+        transform.Translate(Vector2.right * xInput * currentSpeed * Time.deltaTime);
+        transform.Translate(Vector2.up * yInput * currentSpeed * Time.deltaTime);
     }
 }
