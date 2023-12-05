bd990922220478fd6f132dc23a210fbe9e76d96a
knightflo <f.van.den.berg@sintlucasedu.nl>
2023-12-05 14:55:41 +0100

    dialogiue
    je moeder is een plopkeok

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,35 @@
+
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEditor.UI;
+//using UnityEngine;
+//using UnityEngine.UI;
+
+
+//public class Dialogue : MonoBehaviour
+//{
+//    private Image image;
+//    public Color color;
+
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+        
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+        
+//    }
+
+//    private void OnTriggerEnter2D(Collider2D collision)
+//    {
+//        if (collision.gameObject.tag == "NPC")
+//        {
+//            image = GetComponent<Image>();
+//            image.color = 255;
+//        }
+//    }
+//}
